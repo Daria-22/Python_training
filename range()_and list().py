34b4fd75444c99012f1a87c12c46d4879785c23a
Daria Kozak <119840398+Daria-22@users.noreply.github.com>
2023-07-04 18:36:33 +0930

    Update range()_and list().py
    Examples of using range() and list()

--- range()_and list().py
+++ range()_and list().py
@@ -1,1 +1,17 @@
+# Your code below: 
+#create range
+number_list = range(9)
+
+#print range (pay attention, output will be 0 and 9)
+print(number_list)
+#Output range(0, 9)
+
+#for printing all the components in the range, use built-in  list() method
+print(list(number_list))
+#Output  [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+##another example:
+zero_to_seven = range(8)
+print(zero_to_seven)
+print(list(zero_to_seven))
 
