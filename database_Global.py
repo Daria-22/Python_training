ec6da2bdbc60924893102c68f54e9c25f84222bb
Daria Kozak <119840398+Daria-22@users.noreply.github.com>
2023-07-11 21:31:09 +0930

    Started the code for Database assignment

---
+++ database_Global.py
@@ -0,0 +1,12 @@
+Class Database:
+    def __init__(database_name):
+        _connected_to_database= False
+    
+    def get_connected_to_database(_connected_to_database)
+    
+    def set_connected_to_database(connection_state):
+        print("Неможливо змінити значення connected_to_database за допомогою присвоєння. Використайте метод connect_to_database")
+        
+    
+    
+    
