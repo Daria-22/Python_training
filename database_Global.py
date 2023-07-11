a3935790381612d07b4934fa4ec3e81e59849d79
Daria Kozak <119840398+Daria-22@users.noreply.github.com>
2023-07-11 21:48:27 +0930

    added a few new lines

--- database_Global.py
+++ database_Global.py
@@ -2,10 +2,14 @@ Class Database:
     def __init__(database_name):
         _connected_to_database= False
     
-    def get_connected_to_database(_connected_to_database)
+    def get_connected_to_database(_connected_to_database):
     
     def set_connected_to_database(connection_state):
         print("Неможливо змінити значення connected_to_database за допомогою присвоєння. Використайте метод connect_to_database")
+    
+    def get_database_name():
+    
+    def set_database_name():
         
     
     
