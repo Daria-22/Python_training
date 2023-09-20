b1499c443a3e8ca419b0f839aef86b1b9896b0cb
Daria Kozak <119840398+Daria-22@users.noreply.github.com>
2023-09-20 16:10:14 +0930

    Odd index find and filter

--- odd_items_filter.py
+++ odd_items_filter.py
@@ -2,13 +2,19 @@
 def odd_indices(my_list):
     index = 1
     odd_list = []
-    length = len(my_list)
-    while index < (len(my_list)):
-       odd_item = my_list[index]
-       odd_list.append(odd_item) 
+    while index < len(my_list):
+       odd_list.append(my_list[index]) 
        index +=2
     return odd_list
 
 
 #Uncomment the line below when your function is done
 print(odd_indices([4, 3, 7, 10, 11, -2]))
+
+
+'''def odd_indices(my_list):
+  new_list = []
+  for index in range(1, len(my_list), 2):
+    new_list.append(my_list[index])
+  return new_list'''
+  
