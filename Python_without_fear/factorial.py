28a40f9048f165e581e43c864aa34e99f88988d7
Daria Kozak <119840398+Daria-22@users.noreply.github.com>
2023-10-13 23:00:29 +1030

    Factorial

--- Python_without_fear/factorial.py
+++ Python_without_fear/factorial.py
@@ -1,6 +1,13 @@
-n = int(input("Input your number"))
-prod = i = 1
-while i <= n:
-    prod *= i
-    i +=1
-print("Factorial of your number equals " + str(prod))
+def main():
+    n = int(input("Input your integer or alternatively enter 0 to quit"))
+    while n != 0:    
+        prod = i = 1
+        i = 2
+        while i <= n:
+            prod *= i
+            i +=1
+        print("Factorial of your number equals " + str(prod))
+        n = int(input("Enter 0 to quit"))
+    print("Bye for now!")
+        
+main()
