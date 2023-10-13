80a84fb59824de9cba4a846f73632925ed773383
Daria Kozak <119840398+Daria-22@users.noreply.github.com>
2023-10-13 22:04:51 +1030

    Factorial and while list with end = ' '

---
+++ Python_without_fear/easy_list_while.py
@@ -0,0 +1,4 @@
+n = 15
+while n <= 40:
+    print(n, end = ' ')
+    n+=1
---
+++ Python_without_fear/factorial.py
@@ -0,0 +1,6 @@
+n = int(input("Input your number"))
+prod = i = 1
+while i <= n:
+    prod *= i
+    i +=1
+print("Factorial of your number equals " + str(prod))
