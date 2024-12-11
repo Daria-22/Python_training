58545d90dc959bd9535301e5b2bb2636cc50a87a
Daria Kozak <119840398+Daria-22@users.noreply.github.com>
2024-12-12 00:07:26 +1100

    Create gather_info.sh
    script_bash_gather_info

---
+++ gather_info.sh
@@ -0,0 +1,18 @@
+#in terminal
+example=hello
+echo $example
+#output hello
+
+line = "-------------------"
+
+echo "Starting at: $(date)";  echo $line
+
+echo "Uptime: $(date)";  uptime; echo $line
+
+echo "Free"; free;  echo $line
+
+echo "Who";  who; echo $line
+
+echo "Finishing at: $(date)";  echo $line
+
+
